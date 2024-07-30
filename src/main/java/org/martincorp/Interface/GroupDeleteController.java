45872c96571629b0007680247aedf2b153180483
THEliberator03 <theliberator03@gmail.com>
2024-07-30 11:20:44 +0200

    Added License notice for all files

--- src/main/java/org/martincorp/Interface/GroupDeleteController.java
+++ src/main/java/org/martincorp/Interface/GroupDeleteController.java
@@ -1,6 +1,21 @@
-package org.martincorp.Interface;
+/**
+ *  NeutronMail client for admintration purposes.
+ *  Copyright (C) 2024 by Martín Marín.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import java.time.LocalDate;
+package org.martincorp.Interface;
 
 import org.martincorp.Database.DBActions;
 import org.martincorp.Model.Group;
--- src/main/java/org/martincorp/Interface/MainController.java
+++ src/main/java/org/martincorp/Interface/MainController.java
@@ -1,3 +1,20 @@
+/**
+ *  NeutronMail client for admintration purposes.
+ *  Copyright (C) 2024 by Martín Marín.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.martincorp.Interface;
 
 import java.net.URL;
@@ -21,14 +38,13 @@ public class MainController {
     @FXML private Label empGroupText;
     @FXML private Label genCertText;
 
-      //Other variables:
+    //Other variables:
     private Stage window;
     private DBActions db;
     private URL imgURL;
 
     //Equivalent to main method when the controller is started:
-    @FXML
-    public void initialize(){
+    @FXML public void initialize(){
         db = new DBActions();
 
         //Setting up the menu icons:
